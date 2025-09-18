2633a0b98f0c19644a0521c31cd03041dd55cb1d
Lokesh Dangare <lokeshdangare05@gmail.com>
2025-09-18 17:38:51 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Lokesh Dangare",
+    author_email="lokeshdangare57@gmail.com",
+    packages=find_packages()
+)
